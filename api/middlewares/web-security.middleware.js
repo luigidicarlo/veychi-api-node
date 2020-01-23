a75a7da979d79eb614c8f9f780e3559b75dfce18
luigidicarlo <lehuertad95@gmail.com>
2020-01-23 17:44:51 -0400

    Added OPTIONS to CORS

--- api/middlewares/web-security.middleware.js
+++ api/middlewares/web-security.middleware.js
@@ -1,6 +1,6 @@
 const allowCors = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE, POST');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE, POST, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 };
